6433de2952abbe3ff67585c65c7d2daff9abc931
Ostoic <dinnertable@hotmail.ca>
2017-07-12 17:27:38 -0400

    Structured windows::system a little

---
+++ windows/system/information.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <windows.h>
+
+namespace distant {
+namespace windows {
+namespace system  {
+
+
+} // end namespace system
+} // end namespace windows
+} // end namespace distant
